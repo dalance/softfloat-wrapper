8d2ae1f9494d70c49c9825deafd751dfcd1b1b6d
msyksphinz <msyksphinz.dev@gmail.com>
2020-10-01 16:48:17 +0900

    Fix softfloat_sys_riscv into softfloat_sys

--- src/f32.rs
+++ src/f32.rs
@@ -73,23 +73,23 @@ impl Float for F32 {
     }
 
     fn eq<T: Borrow<Self>>(&self, x: T) -> bool {
-        unsafe { softfloat_sys_riscv::f32_eq(self.0, x.borrow().0) }
+        unsafe { softfloat_sys::f32_eq(self.0, x.borrow().0) }
     }
 
     fn lt<T: Borrow<Self>>(&self, x: T) -> bool {
-        unsafe { softfloat_sys_riscv::f32_lt(self.0, x.borrow().0) }
+        unsafe { softfloat_sys::f32_lt(self.0, x.borrow().0) }
     }
 
     fn le<T: Borrow<Self>>(&self, x: T) -> bool {
-        unsafe { softfloat_sys_riscv::f32_le(self.0, x.borrow().0) }
+        unsafe { softfloat_sys::f32_le(self.0, x.borrow().0) }
     }
 
     fn lt_quiet<T: Borrow<Self>>(&self, x: T) -> bool {
-        unsafe { softfloat_sys_riscv::f32_lt_quiet(self.0, x.borrow().0) }
+        unsafe { softfloat_sys::f32_lt_quiet(self.0, x.borrow().0) }
     }
 
     fn le_quiet<T: Borrow<Self>>(&self, x: T) -> bool {
-        unsafe { softfloat_sys_riscv::f32_le_quiet(self.0, x.borrow().0) }
+        unsafe { softfloat_sys::f32_le_quiet(self.0, x.borrow().0) }
     }
 
     fn compare<T: Borrow<Self>>(&self, x: T) -> Option<Ordering> {
--- src/f64.rs
+++ src/f64.rs
@@ -73,23 +73,23 @@ impl Float for F64 {
     }
 
     fn eq<T: Borrow<Self>>(&self, x: T) -> bool {
-        unsafe { softfloat_sys_riscv::f64_eq(self.0, x.borrow().0) }
+        unsafe { softfloat_sys::f64_eq(self.0, x.borrow().0) }
     }
 
     fn lt<T: Borrow<Self>>(&self, x: T) -> bool {
-        unsafe { softfloat_sys_riscv::f64_lt(self.0, x.borrow().0) }
+        unsafe { softfloat_sys::f64_lt(self.0, x.borrow().0) }
     }
 
     fn le<T: Borrow<Self>>(&self, x: T) -> bool {
-        unsafe { softfloat_sys_riscv::f64_le(self.0, x.borrow().0) }
+        unsafe { softfloat_sys::f64_le(self.0, x.borrow().0) }
     }
 
     fn lt_quiet<T: Borrow<Self>>(&self, x: T) -> bool {
-        unsafe { softfloat_sys_riscv::f64_lt_quiet(self.0, x.borrow().0) }
+        unsafe { softfloat_sys::f64_lt_quiet(self.0, x.borrow().0) }
     }
 
     fn le_quiet<T: Borrow<Self>>(&self, x: T) -> bool {
-        unsafe { softfloat_sys_riscv::f64_le_quiet(self.0, x.borrow().0) }
+        unsafe { softfloat_sys::f64_le_quiet(self.0, x.borrow().0) }
     }
 
     fn compare<T: Borrow<Self>>(&self, x: T) -> Option<Ordering> {
